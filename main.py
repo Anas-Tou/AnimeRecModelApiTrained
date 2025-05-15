8b7888a3c5d9118aead837a03cff4f838c94f72d
Anas Touari <163757687+Anas-Tou@users.noreply.github.com>
2025-05-15 21:20:37 +0100

    Fix CORMiddleware

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ origins = [
 ]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:8070", "http://localhost:8070"],  # Specific origins for your client
+    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],  # Specific origins for your client
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
